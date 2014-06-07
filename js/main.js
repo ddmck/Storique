4e60062d247af0c3802302969b9afe457cdce04d
Greta Kaufeld <brunobaufeld@Brunos-MacBook-Pro.local>
2014-06-07 17:10:38 +0100

    early prototyping of decision navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
+
+	var imageSelection = function(){
+		var weather = $(".weatherChoice").html();
+		var house = "";
+		var hair = "";
+		var image_name = weather+house+hair+".jpeg";
+		alert(image_name);
+		
+	}
+	imageSelection();
+	
 	$("#chooseSunny").on("click",function(){
 		$(".weatherChoice").html("sunny");
+		imageSelection();
+		
 	});
+	
+	$("#chooseCloudy").on("click",function(){
+		$(".weatherChoice").html("cloudy");
+		imageSelection();
+	
+	});
+	
+	$(".chooseStone").on("click",function(){
+		$(".houseChoice").html("stone");
+		
+	});
+	
+	$(".chooseWood").on("click",function(){
+		$(".houseChoice").html("wooden");
+		
+	});
+	
+	$(".chooseRed").on("click",function(){
+		$(".hairChoice").html("red");
+		
+	});
+	
+	$(".chooseGreen").on("click",function(){
+		$(".hairChoice").html("green");
+		
+	});
+	
+	
 });
+
+
